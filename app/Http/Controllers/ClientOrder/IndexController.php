6ef1dee4b35de07759acaa2acfed87dd56cbb144
daelonCHIck <OchKrut1337>
2022-12-07 13:24:38 +0300

    Dima eblan

--- app/Http/Controllers/ClientOrder/IndexController.php
+++ app/Http/Controllers/ClientOrder/IndexController.php
@@ -9,7 +9,7 @@ class IndexController extends Controller
 {
 
     public function __invoke()
-    {
+    {   //gfdgfdgdfgd
         $user = auth()->user();
         // Заказы пользователя
         $orders = $user->clientOrders;
