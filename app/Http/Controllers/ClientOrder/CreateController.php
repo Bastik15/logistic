3899ff384c8c7451af42436834bf307ee47810ee
daelonCHIck <OchKrut1337>
2022-12-07 13:03:27 +0300

    Create comment

--- app/Http/Controllers/ClientOrder/CreateController.php
+++ app/Http/Controllers/ClientOrder/CreateController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 
 class CreateController extends Controller
 {
+    //fghfghfghf
     public function __invoke()
     {
         return view('clientOrder.create');
